3d3c03e6f50a35338018e4a567032ddd742ae7d0
daniyar <daniar.kulsin@gmail.com>
2021-11-27 20:48:36 +0600

    first commit

---
+++ js/scrypt.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// const obj = {
+//     name: "daniyar",
+//     age: 24,
+//     isMarried: false
+// };
+
+// console.log(obj);
+
+// alert("Hello world");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+
+// const awnser = +prompt("Вам есть 18?", "18");
+// console.log(awnser + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Cколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+
+
+
+
+//Инкапсуляция 
+
+// const category = 'toys';
+
+// console.log(`https://somueurl.com/${category}/5`);
+
+// const user = "daniyar";
+
+// alert(`Привет, ${user}`);
+
+
+
